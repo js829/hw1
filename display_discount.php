e0971bc0b364a75ce9312732e913886dac288b2f
js829 <js829@njit.edu>
2017-02-03 23:34:25 -0500

    add display discount php file

--- display_discount.php
+++ display_discount.php
@@ -1,7 +1,25 @@
-<!DOCTYPE html>
-<html> 
-<head>
+<?php
+	// get the data from the form
+	$product_description = $_POST['product_description'];
+	$list_price = $_POST['list_price'];
+	$discount_percent = $_POST['discount_percent'];
 
+	// calculate the discount
+	$discount = $list_price * $discount_percent * .01;
+	$discount_price = $list_price - $discount;
+
+	// apply currency formatting to the dollar and percent amounts
+	$list_price_formatted = "$".number_format($list_price, 2);
+	$discount_percent_formatted = $discount_percent."%";
+	$discount_formatted = "$".number_format($discount, 2);
+	$discount_price_formatted = "$".number_format($discount_price, 2);
+
+	// escape the unformatted input
+	$product_description_escaped = htmlspecialchars($product_description);
+?>
+<!DOCTYPE html>
+<html>
+<head>
 	<title>Product Discount Calculator</title>
 	<link rel="stylesheet" type="text/css" href="main.css">
 </head>
@@ -10,19 +28,21 @@
 		<h1>Product Discount Calculator</h1>
 
 		<label>Product Description:</label>
-		<span>Guitar</span><br>
+		<span><?php echo $product_description_escaped; ?></span><br>
 
-		<label>List price:</label>
-		<span>$1,250.00</span><br>
+		<label>List Price:</label>
+		<span><?php echo $list_price_formatted; ?></span><br>
 
 		<label>Standard Discount:</label>
-		<span>13%</span><br>
+		<span><?php echo $discount_percent_formatted; ?></span><br>
 
-		<label>Discount Amount</label>
-		<span>162.50</span><br>
+		<label>Discount Amount:</label>
+		<span><?php echo $discount_formatted; ?></span><br>
 
-		<label>Discount Price</label>
-		<span>$1,087.50</span><br>
+		<label>Discount Price:</label>
+		<span><?php echo $discount_price_formatted; ?></span><br>
+
 	</main>
 </body>
 </html>
+
