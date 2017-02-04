1c829bd64237dbc2928b27b98ac8971d26e03f48
js829 <js829@njit.edu>
2017-02-03 19:10:47 -0500

    add response page

---
+++ display_discount.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html> 
+<head>
+
+	<title>Product Discount Calculator</title>
+	<link rel="stylesheet" type="text/css" href="main.css">
+</head>
+<body>
+	<main>
+		<h1>Product Discount Calculator</h1>
+
+		<label>Product Description:</label>
+		<span>Guitar</span><br>
+
+		<label>List price:</label>
+		<span>$1,250.00</span><br>
+
+		<label>Standard Discount:</label>
+		<span>13%</span><br>
+
+		<label>Discount Amount</label>
+		<span>162.50</span><br>
+
+		<label>Discount Price</label>
+		<span>$1,087.50</span><br>
+	</main>
+</body>
+</html>
